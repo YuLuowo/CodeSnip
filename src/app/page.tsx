0eaf42fc0b9afb6eeac706be7fc2dc246429cae1
YuLuowo <diego050415@gmail.com>
2025-10-08 20:15:56 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { ThemeToggle } from "@/components/layout/theme-toggle";
+
 export default function Home() {
     return (
-        <>
-        </>
-    );
+        <main className="flex flex-col items-center justify-center min-h-screen gap-4">
+            <h1 className="text-3xl font-bold">👋 Test</h1>
+            <ThemeToggle />
+        </main>
+    )
 }
